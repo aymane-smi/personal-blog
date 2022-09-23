e66b7793433ae54908fd781c3dfb4355f7bf7b41
aymane <aymanebel2@outlook.fr>
2022-09-23 16:05:33 +0100

    add authoriation middleware

---
+++ go-api/middleware/authorization.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"os"
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func Authorization(c *fiber.Ctx) error {
+	godotenv.Load()
+	token := strings.Split(string(c.Context().Request.Header.Peek("Authorization")), "")[1]
+
+	_, err := jwt.ParseWithClaims(token, &jwt.StandardClaims{}, func(t *jwt.Token) (interface{}, error) {
+		return []byte(os.Getenv("SECRET_KEY")), nil
+	})
+
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "inavlid token",
+		})
+	}
+	return c.Next()
+}
