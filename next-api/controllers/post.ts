fcaf6892589077d2c7febd46e0fb1dd63e4e12ed
aymane <aymanebel2@outlook.fr>
2022-11-07 00:38:49 +0100

    upload done

--- next-api/controllers/post.ts
+++ next-api/controllers/post.ts
@@ -16,15 +16,19 @@ export const createPost = async (req: NextApiRequest, res: NextApiResponse<msgEr
 
     console.log("inside the create controller");
     let msg = await uploadLogic(req)
-    console.log(msg);
+    res.json({
+        message: "created!",
+    })
     //const post = Post.create(req.body);
 };
 
 
 const uploadLogic = async (req: NextApiRequest)=>{
     return new Promise((resolve, reject)=>{
-        const form = new IncomingForm({multiples: true, uploadDir: './public/images'});
-        form.keepExtensions = true;
+        const form = new IncomingForm({multiples: true, uploadDir: './public/images', keepExtension: true, filename: (name, ext, part, form)=>{
+            console.log(name);
+            return name;
+        }});
         form.parse(req, (err, fields, files)=>{
             if(err)
                 return reject(err);
